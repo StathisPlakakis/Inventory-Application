d242cfda3c98e77e8fc34ecd78584c9a1f97fd7f
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-11-23 11:55:30 +0200

    Add the script to create the relational tables in my db

---
+++ db/populatedb.js
@@ -0,0 +1,38 @@
+const {Client} = require('pg');
+require('dotenv').config();
+
+const SQL = `
+  CREATE TABLE IF NOT EXISTS boats (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  category_id INTEGER,
+  brand_id INTEGER,
+  title VARCHAR,
+  price INTEGER,
+  description VARCHAR,
+  images TEXT[],
+  date VARCHAR,
+  views INTEGER
+  );
+
+  CREATE TABLE IF NOT EXISTS categories (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  category VARCHAR (30)
+  );
+
+  CREATE TABLE IF NOT EXISTS brand (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  brand VARCHAR (50)
+  );
+`;
+
+async function main() {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL
+  });
+
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+}
+
+main();
