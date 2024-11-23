9ff3b18acad651e1001a3e66075f051a596cf1c2
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-11-23 17:57:57 +0200

    Add the header

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = 3000;
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 
 app.listen(PORT, () => {
