b9aa851406f3c344c120940b23c4f276c78d621d
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-11-24 16:37:27 +0200

    Pass the list of tables of db inside dashboard

--- db/queries.js
+++ db/queries.js
@@ -3,7 +3,15 @@ const pool = require('./pool');
 
 const getAllTables = async () => {
   const {rows} = await pool.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_name != 'boats';");
-  return rows.map(row => row.table_name);
+  const result = await Promise.all(
+    rows.map(async (row) => {
+    const table_name = row.table_name;
+    const total_items_result =  await pool.query(`SELECT COUNT(*) AS total FROM ${table_name}`);
+    const total_items = total_items_result.rows[0].total;
+    return {table_name, total_items}
+    })
+  )
+  return result;
 }
 
 module.exports = {
