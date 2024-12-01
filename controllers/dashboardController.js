0b974e1b94d83ba65e0dbcd53461b589845abcb0
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-12-01 18:51:38 +0200

    Add some basic validation to boat creation

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,6 +1,9 @@
 const db = require('../db/queries');
 const asyncHandler = require("express-async-handler");
 const {body, validationResult} = require('express-validator');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs/promises');
 
 const dashboardRouterGet = asyncHandler(async (req, res) => {
   const active_table = req.query.active;
@@ -78,17 +81,51 @@ const createBrand = [validateBrand, asyncHandler(async (req, res) => {
   res.redirect('/dashboard?active=brands');
 })]
 
-const createBoat = asyncHandler(async (req, res) => {
-  const { category, brand, title, price, description } = req.body;
-  const file = req.file;
-  const category_id = await db.getCategoryId(category);
-  const brand_id = await db.getBrandId(brand);
-  const boatResult = await db.addNewBoat(category_id, brand_id, title, price, description);
-  const boatId = boatResult.rows[0].id;
-  await db.addNewImages(boatId, file.buffer);
-  res.redirect('/dashboard?active=boats');
+const validateBoat = [
+  body('price').trim()
+    .not().isEmpty().withMessage('Price is required')
+    .isInt().withMessage('Price should be an integer')
+    .isInt({ min: 1, max: 999999999 }).withMessage('Price must be a positive integer less than 1,000,000,000')
+]
 
-})
+const createBoat = [
+  validateBoat,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const { category, brand, title, price, description } = req.body;
+      const tables = await db.getAllTables();
+      const table_rows = await db.getAllTableRows('boats');
+      const table_rows_categories = await db.getAllTableRows('categories');
+      const table_rows_brands = await db.getAllTableRows('brands');
+
+      return res.render('dashboardError', {
+        tables,
+        active_table: 'boats',
+        table_rows,
+        table_rows_categories,
+        table_rows_brands,
+        errors: errors.array(),
+        invalid_category: category,
+        invalid_brand: brand,
+        invalid_title: title,
+        invalid_description: description,
+        invalid_price: price,
+      });
+    }
+    const { category, brand, title, price, description } = req.body;
+    const file = req.file;
+    const category_id = await db.getCategoryId(category);
+    const brand_id = await db.getBrandId(brand);
+    const boatResult = await db.addNewBoat(category_id, brand_id, title, price, description);
+    if (file) {
+      const boatId = boatResult.rows[0].id;
+      const fileBuffer = file.buffer;
+      await db.addNewImages(boatId, fileBuffer);
+    }
+    res.redirect('/dashboard?active=boats');
+  },
+];
 
 module.exports = {
   dashboardRouterGet,
